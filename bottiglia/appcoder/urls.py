d3a9c520040c1423b0f33497e1fad8c034f00089
Matias Salinas <matixxdpls@gmail.com>
2022-11-02 20:02:44 -0300

    added new urls

---
+++ bottiglia/appcoder/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from appcoder.views import *
+urlpatterns = [
+    path('estudiantes',estudiantes),
+    path('profesores/',profesores),
+    path('cursos/',cursos),
+    path('entregables',entregables),
+
+]
--- bottiglia/bottiglia/urls.py
+++ bottiglia/bottiglia/urls.py
@@ -14,9 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from bottiglia.views import *
-from appcoder.views import *
+
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('saludo/',vista_saludo),
@@ -25,5 +27,5 @@ urlpatterns = [
     path('plantilla/',vista_plantilla),
     path('alumnos/',vista_listado_alumnos),
     path('alumnos2/',vista_listado_alumnos2),
-    path("cursos/",listado_cursos)
+    path('coder/',include("appcoder.urls")),
 ]
